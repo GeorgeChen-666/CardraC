01de64e4649d3b099307ca1128029afa6afaf580
GeorgeChen <a@b.c>
2024-04-19 17:36:27 +0800

    change npm commands

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 const { app, BrowserWindow, dialog, ipcMain } = require('electron');
-
+const isDev = require('electron-is-dev')
 let mainWindow;
 
 function createWindow() {
@@ -12,7 +12,7 @@ function createWindow() {
       contextIsolation: false,
     },
   });
-  mainWindow.loadURL('http://localhost:3000/');
+  mainWindow.loadURL(isDev?'http://localhost:3000/':`file://${path.join(__dirname, '../build/index.html')}`);
 }
 app.whenReady().then(() => {
   createWindow()
